aaaebb343bcc764ffc4cd572c475b39c04f8c662
Liam Shaw <liam.philip.shaw@gmail.com>
2022-06-22 15:53:47 +0200

    now works from this dir, takes args

---
+++ scripts/get-gene-cluster-accessions.py
@@ -0,0 +1,73 @@
+# downloads all chromosomes/plasmids which have a gene present in them
+# going off a cluster of genomes
+# conda env: scipy
+
+
+import pandas as pd
+import scipy.cluster.hierarchy as hc
+import argparse
+import warnings
+warnings.filterwarnings("ignore") # because seaborn gives warning as we are passing a distance matrix, even though we are doing this deliberately
+
+DATA_DIR = '../data-processing/'
+def get_options():
+    parser = argparse.ArgumentParser(description='download all sequences containing a sequence variant similar to a named beta-lactamase')
+    parser.add_argument('family', help='Name of enzyme family (e.g. CTX-M)', type=str)
+    parser.add_argument('gene', help='Name of sequence (e.g. CTX-M-55)', type=str)
+    parser.add_argument('threshold', help='Distance threshold (diffs in clustalo MSA)', type=int)
+    return parser.parse_args()
+
+
+def getNeighbours(family_name, gene_name, distance=10):
+    """get the neighbours using clustering within a certain distance threshold of the gene"""
+    snps = pd.read_csv(DATA_DIR+'/'+family_name+'_diffs_aln.tsv', sep='\t', index_col=0)
+    #plt.close()
+    df = snps
+    # and cluster
+    linkage_mat = hc.linkage(df, method='average')
+    # Get clusters
+    maximum_distance = distance
+    clusters = hc.fcluster(linkage_mat, maximum_distance, criterion='distance')
+    cluster_of_gene = clusters[list(df.index).index(gene_name)]
+    names = [list(df.index)[i] for i in [x for x in range(len(clusters)) if clusters[x]==cluster_of_gene]]
+    return(names)
+
+def main():
+    args = get_options()
+    pa_df = pd.read_csv(DATA_DIR+'/CARD-all-hits-presence-absence-chrom-plasmid.csv',index_col=0)
+
+    genome_df = pd.read_csv('../CARD/card-genomes.txt', sep='\t', index_col=0)
+    # Gene clusters
+    gene_cluster = getNeighbours(args.family, args.gene, args.threshold)
+
+    # Dict of accessions and whether chromosome/plasmid
+    accessions = {}
+    # Plasmid accessions
+    plasmids = list(genome_df[genome_df['data_source']=='ncbi_plasmid'].index)
+    plasmid_df = pa_df.loc[plasmids]
+
+    for g in gene_cluster:
+        if g in plasmid_df.columns:
+            results = list(plasmid_df.index[plasmid_df[g]!=0])
+            for r in results:
+                if r not in accessions.keys():
+                    accessions[r] = 'plasmid'
+
+    # Chromosome accessions
+    chroms = list(genome_df[genome_df['data_source']=='ncbi_chromosome'].index)
+
+    chrom_df = pa_df.loc[chroms]
+    for g in gene_cluster:
+        if g in chrom_df.columns:
+            results = list(chrom_df.index[chrom_df[g]!=0])
+            for r in results:
+                if r not in accessions.keys():
+                    accessions[r] = 'chromosome'
+
+    # Write to file
+    with open(args.gene+'_within_'+str(args.threshold)+'_diffs_accs.txt', 'w') as f:
+        for k, v in accessions.items():
+            _ = f.write('%s,%s\n' % (k, v))
+
+if __name__ == "__main__":
+    main()
