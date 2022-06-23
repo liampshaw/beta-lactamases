375737967a573fea44f8943cf70d2d7ab003e8fe
Liam Shaw <liam.philip.shaw@gmail.com>
2022-06-23 15:10:45 +0200

    tweak: use mafft rather than clustalo

--- scripts/03a_align-families.sh
+++ scripts/03a_align-families.sh
@@ -4,8 +4,7 @@ while read gene;
 do
 	echo $gene
 	grep -A 1 $gene ../CARD/nucleotide_fasta_protein_homolog_model.fasta  | awk '$1!="--"' | sed -e 's/.*|/>/g' | sed -e 's/ .*//g' > ../data-processing/"$gene".fasta
-	conda activate clustalo
-	clustalo -i ../data-processing/"$gene".fasta -o ../data-processing/"$gene".aln
-	conda activate snp-dists
+	#clustalo -i ../data-processing/"$gene".fasta -o ../data-processing/"$gene".aln
+	mafft --auto ../data-processing/"$gene".fasta > ../data-processing/"$gene".aln
 	snp-dists -a -b ../data-processing/"$gene".aln > ../data-processing/"$gene"_diffs_aln.tsv # differences including ins/del (not just SNPs)
 done < ../beta-lactamases.txt
