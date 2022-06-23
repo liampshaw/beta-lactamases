eb808698a429f4ba8a77ef74ea0133db9ab9ad5b
Liam Shaw <liam.philip.shaw@gmail.com>
2022-06-23 10:35:47 +0200

    handles multiple blast hits

--- scripts/extract-region.py
+++ scripts/extract-region.py
@@ -10,6 +10,7 @@ import re
 import logging
 import numpy as np
 import argparse
+import glob
 
 
 def get_options():
@@ -84,9 +85,7 @@ def blast_search(query_fasta, db_fasta):
     blast_output = re.split('\n|\t',blast_out.decode()) # Decode
     #print(blast_output)
     logging.debug('\nRemoving temporary blast databases...')
-    os.remove(db_fasta+'.nin')
-    os.remove(db_fasta+'.nhr')
-    os.remove(db_fasta+'.nsq')
+    [os.remove(x) for x in glob.glob(db_fasta+'.n*')]
     # Looking through to cut out upstream region
     if blast_output == ['']:
         logging.debug('\nNo blast hit for gene!')
@@ -96,22 +95,46 @@ def blast_search(query_fasta, db_fasta):
         blast_results = np.reshape(blast_output, (-1, 12))
         contigs_with_hits = list(blast_results[:,1])
         contigs_one_hit = [x for x in set(contigs_with_hits) if contigs_with_hits.count(x)==1]
-        results_dict = {result[1]: [int(result[8]), int(result[9])] for result in blast_results if result[1] in contigs_one_hit} # only keep contigs with exactly one hit
-        return(results_dict)
+        contigs_multiple_hits = [x for x in set(contigs_with_hits) if contigs_with_hits.count(x)>1]
+        results_dict_one_hit = {result[1]: [int(result[8]), int(result[9])] for result in blast_results if result[1] in contigs_one_hit} # only keep contigs with exactly one hit
+        results_dict_multiple_hit = {x: 'multiple_hits' for x in contigs_multiple_hits}
+        return({**results_dict_one_hit, **results_dict_multiple_hit})
 
-def extract_regions(starting_fasta, blast_hits, upstream_bases=1000, downstream_bases=1000):
+def extract_regions(starting_fasta, blast_hits, gene_length, is_circular=False, upstream_bases=1000, downstream_bases=1000):
     '''extracts the regions from a fasta file'''
     sequences = read_fasta(starting_fasta)
+    # Check for sequences with >1 hit in blast hits
     extracted_seqs = {}
+
     for seq_id, coords in blast_hits.items():
-        contig_seq = str(sequences[seq_id].seq)
-        #print(region_seq)
-        if coords[0]<coords[1]: # positive strand
-            limits = [max(0, coords[0]-1-upstream_bases), min(coords[1]+downstream_bases, len(contig_seq))]
-            extracted_seqs[seq_id] = contig_seq[limits[0]:limits[1]]
-        elif coords[0]>coords[1]: # negative strand
-            limits = [max(0, coords[1]-1-downstream_bases), min(coords[0]+upstream_bases, len(contig_seq))]
-            extracted_seqs[seq_id] = reverse_complement(contig_seq[limits[0]:limits[1]])
+        if coords=="multiple_hits":
+            print('WARNING: Contig '+seq_id+' has multiple blast hits. Not including in output.')
+        else:
+            contig_seq = str(sequences[seq_id].seq)
+            # Check the length
+            if len(contig_seq)<(upstream_bases+downstream_bases+gene_length):
+                print('WARNING: Contig '+seq_id+' is shorter than flanking region requested!')
+                if coords[0]<coords[1]:
+                    extracted_seqs[seq_id] = contig_seq
+                elif coords[0]>coords[1]:
+                    extracted_seqs[seq_id] = reverse_complement(contig_seq)
+            #print(region_seq)
+            else:
+                triplicate_seq = contig_seq+contig_seq+contig_seq
+                if coords[0]<coords[1]: # positive strand
+                    if not is_circular:
+                        limits = [max(0, coords[0]-1-upstream_bases), min(coords[1]+downstream_bases, len(contig_seq))]
+                        extracted_seqs[seq_id] = contig_seq[limits[0]:limits[1]]
+                    elif is_circular:
+                        limits = [max(coords[1], coords[0]-1-upstream_bases+len(contig_seq)), min(coords[0]+2*len(contig_seq), coords[1]+downstream_bases+len(contig_seq))]
+                        extracted_seqs[seq_id] = triplicate_seq[limits[0]:limits[1]]
+                elif coords[0]>coords[1]: # negative strand
+                    if not is_circular: # also put in a max thing here
+                        limits = [max(0, coords[1]-1-downstream_bases), min(coords[0]+upstream_bases, len(contig_seq))]
+                        extracted_seqs[seq_id] = reverse_complement(contig_seq[limits[0]:limits[1]])
+                    elif is_circular:
+                        limits = [max(coords[0], coords[1]-1-downstream_bases+len(contig_seq)), min(coords[1]+2*len(contig_seq), coords[0]+upstream_bases+len(contig_seq))]
+                        extracted_seqs[seq_id] = reverse_complement(triplicate_seq[limits[0]:limits[1]])
     return extracted_seqs
 
 def main():
@@ -119,28 +142,32 @@ def main():
     input_fasta = args.input
     output_fasta = args.output
     gene_fasta = args.gene
-    are_circular = args.circular
     # Get length of gene
     gene_seq = read_fasta(gene_fasta)
     if len(gene_seq)>1:
-        print('Your gene of interest has more than one occurrence in an input sequence! Check the fasta.')
+        print('The fasta containing your gene of interest has more than sequence! Check the fasta.')
         exit()
     else:
         gene_length = [len(str(gene_seq[v].seq)) for v in gene_seq][0]
-    print('gene length is: ', str(gene_length))
+    #print('gene length is: ', str(gene_length))
 
     results = blast_search(gene_fasta, input_fasta)
-    extractions = extract_regions(input_fasta, results, upstream_bases=int(args.upstream), downstream_bases=int(args.downstream))
+    #print(results)
+    extractions = extract_regions(input_fasta, results, gene_length=gene_length, is_circular=args.circular, upstream_bases=int(args.upstream), downstream_bases=int(args.downstream))
+    N_seqs_written = 0
     with open(output_fasta, 'w') as output_file:
         for k, v in extractions.items():
-            print(k+',', len(v), 'bases extracted around gene.')
+            #print(k+',', len(v)-gene_length, 'bases extracted around gene', '('+str(len(v))+' total)')
             if args.complete==False:
-                output_file.write('>%s\n%s\n' % (k, v))
-                print("...writing to file.")
+                output_file.write('>%s %sbp\n%s\n' % (k, str(len(v)), v))
+                N_seqs_written += 1
+                #print("...writing to file.")
             elif len(v)>(int(args.upstream)+gene_length+int(args.downstream)-1):
-                if "n" not in v:
-                    print("...writing to file.")
-                    output_file.write('>%s\n%s\n' % (k, v))
+                if "n" not in v and "N" not in v: # check for ambiguous characters
+                    #print("...writing to file.")
+                    output_file.write('>%s %sbp\n%s\n' % (k, str(len(v)), v))
+                    N_seqs_written += 1
+    print("FINISHED: Extracted "+str(N_seqs_written)+" regions (from contigs with one blast hit for gene).")
 
 if __name__ == "__main__":
     main()
