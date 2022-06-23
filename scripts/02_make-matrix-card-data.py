ba5310af70d98f9cfe3f694f1ef7ee389f005c91
Liam Shaw <liam.philip.shaw@gmail.com>
2022-06-23 19:25:41 +0200

    write prevalences for chrom/plas separately

--- scripts/02_make-matrix-card-data.py
+++ scripts/02_make-matrix-card-data.py
@@ -22,19 +22,33 @@ df_chrom = df[df['data_source']=='ncbi_chromosome' ]
 df_plas = df[df['data_source']=='ncbi_plasmid' ]
 
 df_chrom_plas = pd.concat([df_chrom, df_plas])
-# make the p/a matrix
-presence_absence = np.zeros((len(df_chrom_plas), len(unique_genes)))
 
-for i in range(0, len(df_chrom_plas)):
-    hits = df_chrom_plas.loc[df_chrom_plas.index[i]]['all_hits_list']
-    for h in hits:
-        if h!='':
-            presence_absence[i, unique_genes.index(h)] = 1
+def makePA(a_df):
+    """makes presence absence matrix from df"""
+    presence_absence = np.zeros((len(a_df), len(unique_genes)))
+    for i in range(0, len(a_df)):
+        hits = a_df.loc[a_df.index[i]]['all_hits_list']
+        for h in hits:
+            if h!='':
+                presence_absence[i, unique_genes.index(h)] = 1
+    # Turn into a dataframe
+    converted_pa_df = pd.DataFrame(presence_absence,
+        index=a_df.index,
+        columns=unique_genes)
+    return converted_pa_df
 
-# Turn into a dataframe
-pa_df = pd.DataFrame(presence_absence,
-    index=df_chrom_plas.index,
-    columns=unique_genes)
+
 
 # Write to file
+pa_df = makePA(df_chrom_plas)
 pa_df.to_csv(DATA_DIR+'CARD-all-hits-presence-absence-chrom-plasmid.csv')
+
+# Make for plasmids and chromosomes
+pa_plas_df = makePA(df_plas)
+pa_plas_df.to_csv(DATA_DIR+'CARD-all-hits-presence-absence-ncbi_plasmid.csv')
+pa_chrom_df = makePA(df_chrom)
+pa_chrom_df.to_csv(DATA_DIR+'CARD-all-hits-presence-absence-ncbi_chromosome.csv')
+# Below not working atm
+#df_contig = df[df['data_source']=='ncbi_contig' ]
+#pa_contig_df = makePA(df_contig)
+#pa_contig_df.to_csv(DATA_DIR+'CARD-all-hits-presence-absence-ncbi_contig.csv')
