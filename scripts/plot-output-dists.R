bae0e873f17a13033a912baee8b72b769f93105f
Liam Shaw <liam.philip.shaw@gmail.com>
2022-06-24 13:59:15 +0200

    remove plot background

--- scripts/plot-output-dists.R
+++ scripts/plot-output-dists.R
@@ -6,15 +6,17 @@ require(cowplot)
 args = commandArgs(trailingOnly=TRUE)
 
 d <- read.csv(args[1],
-              header=T, 
+              header=T,
               stringsAsFactors = F)
 
-d$snps.categorical <- sapply(d$snps, function(x) 
-  ifelse(x<8, x, "8 or more"))
+d$snps.categorical <- sapply(d$snps, function(x)
+  ifelse(x<8, x, ">7"))
+d$snps.categorical <- ordered(d$snps.categorical,
+                      levels=c(seq(0,7), ">7"))
 
 dist.max <- max(c(d$dist.up, d$dist.down))
 
-p.upstream <-  ggplot(d, aes( -dist.up, group=as.factor(snps.categorical), colour=as.factor(snps.categorical), y = 1 - ..y..))+
+p.upstream <-  ggplot(d, aes( -dist.up, group=snps.categorical, colour=snps.categorical, y = 1 - ..y..))+
   stat_ecdf()+
   theme_bw()+
   theme(legend.position = "none")+
@@ -23,8 +25,16 @@ p.upstream <-  ggplot(d, aes( -dist.up, group=as.factor(snps.categorical), colou
   xlab("distance from gene")+
   theme(panel.grid = element_blank())+
   scale_color_brewer(palette="RdYlBu")+
-  ggtitle("Upstream")
-p.downstream <- ggplot(d, aes( dist.down,group=as.factor(snps.categorical), colour=as.factor(snps.categorical)))+
+  ggtitle("Upstream")+
+  theme(axis.line.x = element_line(colour = "black"),
+    axis.line.y=element_line(colour = "black"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank())+
+  xlim(c(-dist.max, 0))+
+  theme(plot.title=element_text(hjust=0.5))
+p.downstream <- ggplot(d, aes( dist.down,group=snps.categorical, colour=snps.categorical))+
   stat_ecdf()+
   theme_bw()+
   labs(colour="SNPs")+
@@ -33,12 +43,23 @@ p.downstream <- ggplot(d, aes( dist.down,group=as.factor(snps.categorical), colo
   theme(panel.grid = element_blank())+
   scale_color_brewer(palette="RdYlBu")+
   xlim(c(0,dist.max))+
-  theme(axis.line.y=element_blank(), 
+  theme(axis.line.y=element_blank(),
         axis.text.y=element_blank(),
         axis.title.y=element_blank())+
-  ggtitle("Downstream")
+  ggtitle("Downstream")+
+  theme(axis.line.x = element_line(colour = "black"),
+    axis.line.y=element_blank(),
+    axis.ticks.y=element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank())+
+    xlim(c(0, dist.max))+
+    theme(plot.title=element_text(hjust=0.5))
+
+
 
 pdf(args[2], width=8, height=4)
-p = cowplot::plot_grid(p.upstream, p.downstream, rel_widths = c(0.7, 1))
+p = cowplot::plot_grid(p.upstream, p.downstream, rel_widths = c(0.8, 1))
 p
 dev.off()
