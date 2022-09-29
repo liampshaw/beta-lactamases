c9ef14d49dab5f85a0c5f8dd2b134b139d7df889
Liam Shaw <liam.philip.shaw@gmail.com>
2022-09-29 14:25:38 +0100

    plot only comparisons involving most prevalent seq

--- scripts/plot-output-dists.R
+++ scripts/plot-output-dists.R
@@ -7,7 +7,7 @@ args = commandArgs(trailingOnly=TRUE)
 # Args:
 # [1] pangraph_all_u5k_d5k.gfa.output_dists.csv 
 # [2] representative comparison sequence: pangraph_all_u5k_d5k.gfa.most_frequent_path_representative.txt 
-
+# [3] deduplicated gene sequences, list: all_u5k_d5k_focal_gene.dedup.txt
 # pangraph_all_u5k_d5k.gfa.unique_paths.txt
 
 d <- read.csv(args[1],
@@ -19,7 +19,7 @@ d$snps.categorical <- sapply(d$snps, function(x)
 d$snps.categorical <- ordered(d$snps.categorical,
                       levels=c(seq(0,7), ">7"))
 
-
+# get representative of the most common path
 representative = read.csv(args[2], header=F, stringsAsFactors=F)$V1
 
 print("before subsetting")
@@ -32,6 +32,14 @@ print(nrow(d.subset))
 print("rows")
 
 
+# Also get most common gene sequence - assuming it exists
+metadata <- read.csv(args[3], 
+                     sep='\t', 
+                     header=F, 
+                     col.names = c("n", "isolates"))
+focal.gene.isolates = strsplit(metadata$isolates[1], split=", ")[[1]]
+
+d.subset.2 = d[which(d$seq1 %in% focal.gene.isolates | d$seq2 %in% focal.gene.isolates),]
 
 
 dist.max <- max(c(d$dist.up, d$dist.down))
@@ -44,7 +52,7 @@ makePlots <- function(df){
       theme(legend.position = "none")+
       labs(colour="SNPs")+
       ylab("cdf")+
-      xlab("distance from gene")+
+      xlab("distance from gene (bp)")+
       theme(panel.grid = element_blank())+
       scale_color_brewer(palette="RdYlBu")+
       ggtitle("Upstream")+
@@ -61,7 +69,7 @@ makePlots <- function(df){
       theme_bw()+
       labs(colour="SNPs")+
       ylab("cdf")+
-      xlab("distance from gene")+
+      xlab("distance from gene (bp)")+
       theme(panel.grid = element_blank())+
       scale_color_brewer(palette="RdYlBu")+
       xlim(c(0,dist.max))+
@@ -91,4 +99,8 @@ dev.off()
 
 pdf(paste0(args[1],'.flanking-plot-output-', representative, '.pdf'), width=8, height=4)
 makePlots(d.subset)
+dev.off()
+
+pdf(paste0(args[1],'.flanking-plot-output-focal-gene-seq.pdf'), width=8, height=4)
+makePlots(d.subset.2)
 dev.off()
