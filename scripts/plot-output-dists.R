d5a028eba64838f2a9d1a73ee580694600e4bbc1
Liam Shaw <liam.philip.shaw@gmail.com>
2022-09-30 15:27:23 +0100

    include NJ tree

--- scripts/plot-output-dists.R
+++ scripts/plot-output-dists.R
@@ -2,6 +2,9 @@
 require(ggplot2)
 require(ggridges)
 require(cowplot)
+require(RColorBrewer)
+require(ape)
+require(ggtree)
 
 args = commandArgs(trailingOnly=TRUE)
 # Args:
@@ -18,6 +21,8 @@ d$snps.categorical <- sapply(d$snps, function(x)
   ifelse(x<8, x, ">7"))
 d$snps.categorical <- ordered(d$snps.categorical,
                       levels=c(seq(0,7), ">7"))
+snps.categorical.colour.palette <- RColorBrewer::brewer.pal(name="RdYlBu", n=9)
+
 
 # get representative of the most common path
 representative = read.csv(args[2], header=F, stringsAsFactors=F)$V1
@@ -54,7 +59,7 @@ makePlots <- function(df){
       ylab("cdf")+
       xlab("distance from gene (bp)")+
       theme(panel.grid = element_blank())+
-      scale_color_brewer(palette="RdYlBu")+
+      scale_color_manual(values=snps.categorical.colour.palette)+
       ggtitle("Upstream")+
       theme(axis.line.x = element_line(colour = "black"),
         axis.line.y=element_line(colour = "black"),
@@ -71,7 +76,7 @@ makePlots <- function(df){
       ylab("cdf")+
       xlab("distance from gene (bp)")+
       theme(panel.grid = element_blank())+
-      scale_color_brewer(palette="RdYlBu")+
+      scale_color_manual(values=snps.categorical.colour.palette)+
       xlim(c(0,dist.max))+
       theme(axis.line.y=element_blank(),
             axis.text.y=element_blank(),
@@ -90,6 +95,53 @@ makePlots <- function(df){
     return(p)
 }
 
+# For tree of SNPs
+treePlot <- function(deduplicated_prefix){
+  dna <- read.dna(paste0(deduplicated_prefix,'.aln'), format='fasta')
+  dist.obj <- ape::dist.dna(dna, model = "N") # distance in SNPs (diffs, actual number)
+  tree <- ape::bionj(dist.obj)
+  
+  metadata <- read.csv(paste0(deduplicated_prefix,'.txt'), 
+                       sep='\t', 
+                       header=F, 
+                       col.names = c("n", "isolates"))
+  rownames(metadata) <- sapply(metadata$isolates, 
+                               function(x) gsub(",.*", "", x))
+  metadata$isolates <- NULL
+  metadata$isolate <- rownames(metadata)
+  
+  metadata.single <- data.frame(isolate=tree$tip.label[which(!tree$tip.label %in% metadata$isolate)],
+                                n=1)
+  
+  metadata <- rbind(metadata, metadata.single)
+  metadata <- metadata[,c("isolate", "n")]
+  rownames(metadata) <- metadata$isolate
+  
+  metadata$focal <- as.factor(c(1, rep(0, nrow(metadata)-1)))# just store whether focal gene or not
+  # Add SNPs compared to focal gene
+  dist.mat <- as.matrix(dist.obj)
+  metadata$SNPs <- dist.mat[metadata$isolate[1],metadata$isolate]
+  metadata$SNPS.categorical <- sapply(metadata$SNPs, function(x)
+    ifelse(x<8, x, ">7"))
+  metadata$categoricalSNPs <- ordered(metadata$SNPS.categorical,
+                                      levels=c(seq(0,7), ">7"))
+  snps.categorical.colour.palette <- RColorBrewer::brewer.pal(name="RdYlBu", n=9)
+  
+  metadata$n.label <- sapply(metadata$n, 
+                             function(x) ifelse(x==1, "", x))
+  
+  p <- ggtree(tree, layout = 'equal_angle')
+  p <- p %<+% metadata+
+    geom_tippoint(aes(size=n, fill=categoricalSNPs), shape=21, colour="black")+
+    #geom_tiplab2(aes(label=n.label), geom="label",size=4, hjust = 0)+
+    scale_fill_manual(values=snps.categorical.colour.palette)+
+    #theme(legend.position = "bottom")+
+    scale_size_continuous(range=c(2, 10), breaks=c(1, 10, 50, 100), name="No. sequences")+
+    geom_treescale(width = 1, offset = 0.1, linesize = 1)+
+    guides(fill=FALSE)+
+    ggtitle("NJ tree of focal gene sequences")
+  return(p)
+}
 
 
 
@@ -101,6 +153,14 @@ pdf(paste0(args[1],'.flanking-plot-output-', representative, '.pdf'), width=8, h
 makePlots(d.subset)
 dev.off()
 
-pdf(paste0(args[1],'.flanking-plot-output-focal-gene-seq.pdf'), width=8, height=4)
-makePlots(d.subset.2)
+deduplicated_prefix = gsub('.txt', '', args[3])
+p.tree <- treePlot(deduplicated_prefix)
+pdf('NJ-gene-tree.pdf')
+p.tree
+dev.off()
+
+pdf(paste0(args[1],'.flanking-plot-output-focal-gene-seq.pdf'), width=10, height=4)
+p.l <- makePlots(d.subset.2)
+p.r <- p.tree
+cowplot::plot_grid(p.l, p.r, rel_widths = c(2, 1))
 dev.off()
