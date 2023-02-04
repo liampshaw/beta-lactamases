0fbc6767d2494ce48f91f7d7a2ec3adcdcd0f09c
Liam Shaw <liam.philip.shaw@gmail.com>
2023-02-04 15:32:59 +0100

    can plot without bandage (confusing script name...)

--- scripts/bandagePlot.py
+++ scripts/bandagePlot.py
@@ -8,11 +8,13 @@ def get_options():
     parser = argparse.ArgumentParser(description='Plots bandage and block plot for given input files.',
                                      prog='bandagePlot')
     parser.add_argument('--prefix', help='Prefix of files to use', required=True)
+    parser.add_argument('--nobandage', help='whether to not do bandage', required=False, action='store_true', default=False)
     parser.add_argument('--bandagewidth', help='Width of Bandage image (px)', required=False, default=7000)
     parser.add_argument('--bandageheight', help='Height of Bandage image (px)', required=False, default=4000)
     parser.add_argument('--width', help='Width of final plot (inches)', required=False, default=7000)
     parser.add_argument('--height', help='Height of final plot (inches)', required=False, default=7000)
     parser.add_argument('--output', help='Output prefix', required=False, default='')
+    parser.add_argument('--metadata', help='whether to use metadata', required=False, action='store_true', default=False)
     return parser.parse_args()
 
 
@@ -31,12 +33,23 @@ def main():
 
     print("Return code:", bandage_output)
     # Plot blocks
+    bandage_string = output_prefix+'_pangraph.gfa.png '
+    if args.nobandage==True:
+        bandage_string = 'none'
+        
     print("## LINEAR PLOT ##")
     with open(prefix+'.gene_block.txt', 'r') as f:
         gene_block = f.readline().strip('')
-    plot_blocks = 'Rscript plot-blocks.R '+prefix+'_pangraph.json.blocks.csv '+\
+    if args.metadata==False:
+        plot_blocks = 'Rscript plot-blocks.R '+prefix+'_pangraph.json.blocks.csv '+\
                                         gene_block+' '+\
-                                        output_prefix+'_pangraph.gfa.png '+\
+                                        bandage_string+' '+\
+                                        output_prefix+'_pangraph_blocks_plot.pdf '+\
+                                        str(args.width)+' '+str(args.height)
+    elif args.metadata==True:
+        plot_blocks = 'Rscript plot-blocks-chromosome-plasmid.R '+prefix+'_pangraph.json.blocks.csv '+\
+                                        gene_block+' '+\
+                                        bandage_string+' '+\
                                         output_prefix+'_pangraph_blocks_plot.pdf '+\
                                         str(args.width)+' '+str(args.height)
     print("Command:", plot_blocks)
