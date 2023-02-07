7db506380ea74bcd9355a80cab1ee4632176e66a
Liam Shaw <liam.philip.shaw@gmail.com>
2023-02-07 14:11:49 +0100

    fix: increase max_target_seqs

--- scripts/blast.sh
+++ scripts/blast.sh
@@ -2,5 +2,5 @@ while read f;
 do
 	makeblastdb -in ../../data/2023-02-05-$f-mmseqs2-polish-u5000-d5000/$f-mmseqs2-polish.all_u5000_d5000.fa -dbtype 'nucl'
 	grep -A 1 $f" " ../data/genes.fasta > tmp.fa
-	blastn -query tmp.fa -db ../../data/2023-02-05-$f-mmseqs2-polish-u5000-d5000/$f-mmseqs2-polish.all_u5000_d5000.fa -outfmt '6 sseqid sstart send' > ../../data/2023-02-05-$f-mmseqs2-polish-u5000-d5000/$f-mmseqs2-polish.all_u5000_d5000.blast_hits_gene.txt 
+	blastn -max_target_seqs 10000 -query tmp.fa -db ../../data/2023-02-05-$f-mmseqs2-polish-u5000-d5000/$f-mmseqs2-polish.all_u5000_d5000.fa -outfmt '6 sseqid sstart send' > ../../data/2023-02-05-$f-mmseqs2-polish-u5000-d5000/$f-mmseqs2-polish.all_u5000_d5000.blast_hits_gene.txt 
 done < ../data/genes.txt
