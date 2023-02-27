a5563f5c4f9b46ef4d2ed8f3b3b8c9ad195e848c
Liam Shaw <liam.philip.shaw@gmail.com>
2023-02-27 16:35:00 +0000

    tryingto make rescomp work

--- scripts/pipeline/run_pipeline_rescomp.sh
+++ scripts/pipeline/run_pipeline_rescomp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-module load R/4.1.0-foss-2021a
+#module load R/4.2.0-foss-2021b 
 module load Anaconda3/2020.11
 eval "$(conda shell.bash hook)"
 conda activate betalactamases
@@ -9,10 +9,27 @@ echo "Running on:"
 echo $MODULE_CPU_TYPE
 echo "at "`date`
 
-export PYTHONPATH=/well/shaw/users/amu125/miniconda/$MODULE_CPU_TYPE/envs/betalactamases/bin/:${PYTHONPATH}
-export PATH=/well/shaw/users/amu125/miniconda/$MODULE_CPU_TYPE/envs/betalactamases/bin/:${PATH}
+if [ $MODULE_CPU_TYPE == "skylake" ]; then
+	module load R/4.2.0-foss-2021b
+fi
 
+if [ $MODULE_CPU_TYPE == "ivybridge" ]; then
+	module load R/4.2.1-foss-2022a
+fi
 
+module load R/4.2.0-foss-2021b 
+#export PYTHONPATH=/well/shaw/users/amu125/miniconda/$MODULE_CPU_TYPE/envs/betalactamases/bin/:${PYTHONPATH}
+#export PATH=/well/shaw/users/amu125/miniconda/$MODULE_CPU_TYPE/envs/betalactamases/bin/:${PATH}
+which python
+#module load Anaconda3/2020.11
+eval "$(conda shell.bash hook)"
+
+# 
+export PATH=/well/shaw/users/amu125/programs/ncbi-blast-2.13.0+/bin:$PATH
+
+# Julia path
+export PATH=$HOME/bin:/usr/local/bin:/well/shaw/users/amu125/programs/julia-1.7.2/bin:/well/shaw/users/amu125/programs/pangraph/bin:$PATH
+export JULIA_DEPOT_PATH="/well/shaw/users/amu125/programs/julia-1.7.2/depot:$JULIA_DEPOT_PATH"
 
 #snakemake -R --until download_CARD_DB --cores 1 --rerun-incomplete
 #snakemake -R --until download_CARD_DB --cores 1 --rerun-incomplete
