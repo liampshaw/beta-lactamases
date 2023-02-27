a60ce9898b401d44ea814deaf959ab814cf8585e
Liam Shaw <liam.philip.shaw@gmail.com>
2023-02-27 15:23:54 +0000

    python path for rescomp

--- scripts/pipeline/run_pipeline_rescomp.sh
+++ scripts/pipeline/run_pipeline_rescomp.sh
@@ -5,6 +5,15 @@ module load Anaconda3/2020.11
 eval "$(conda shell.bash hook)"
 conda activate betalactamases
 
+echo "Running on:"
+echo $MODULE_CPU_TYPE
+echo "at "`date`
+
+export PYTHONPATH=/well/shaw/users/amu125/miniconda/$MODULE_CPU_TYPE/envs/betalactamases/bin/:${PYTHONPATH}
+export PATH=/well/shaw/users/amu125/miniconda/$MODULE_CPU_TYPE/envs/betalactamases/bin/:${PATH}
+
+
+
 #snakemake -R --until download_CARD_DB --cores 1 --rerun-incomplete
 #snakemake -R --until download_CARD_DB --cores 1 --rerun-incomplete
 snakemake --cores 2 --rerun-incomplete
