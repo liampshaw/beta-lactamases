6afad39ddcd5f88d0dead85b116890c98b292fdd
Liam Shaw <liam.philip.shaw@gmail.com>
2023-02-28 10:19:04 +0000

    more python checking

--- scripts/pipeline/run_pipeline_rescomp.sh
+++ scripts/pipeline/run_pipeline_rescomp.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 #module load R/4.2.0-foss-2021b 
+echo "aat start of script"
+which python
+
 module load Anaconda3/2020.11
 eval "$(conda shell.bash hook)"
 conda deactivate
-conda activate betalactamases
+echo "after conda deactivate"
+which python
 
+conda activate betalactamases
+echo "after conda activate betalactamases"
+which python
 echo "Running on:"
+
 echo $MODULE_CPU_TYPE
 echo "at "`date`
 
@@ -24,15 +32,20 @@ fi
 #which python
 #module load Anaconda3/2020.11
 #eval "$(conda shell.bash hook)"
-which python
+
 
 
 # 
 export PATH=/well/shaw/users/amu125/programs/ncbi-blast-2.13.0+/bin:$PATH
+echo "after add blast to path"
+which python
 
 # Julia path
 export PATH=$HOME/bin:/usr/local/bin:/well/shaw/users/amu125/programs/julia-1.7.2/bin:/well/shaw/users/amu125/programs/pangraph/bin:$PATH
 export JULIA_DEPOT_PATH="/well/shaw/users/amu125/programs/julia-1.7.2/depot:$JULIA_DEPOT_PATH"
+
+echo "after julia paths"
+which python
 
 #snakemake -R --until download_CARD_DB --cores 1 --rerun-incomplete
 #snakemake -R --until download_CARD_DB --cores 1 --rerun-incomplete
