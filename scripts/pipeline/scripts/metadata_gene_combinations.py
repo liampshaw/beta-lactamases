99b5845ade8478eb1a80c6555d720a664367f1ae
Liam Shaw <liam.philip.shaw@gmail.com>
2023-03-01 16:22:35 +0000

    extract CARD ids of each gene separately

--- scripts/pipeline/scripts/metadata_gene_combinations.py
+++ scripts/pipeline/scripts/metadata_gene_combinations.py
@@ -2,21 +2,23 @@ import pandas as pd
 
 metadata = pd.read_csv(snakemake.input.metadata, index_col=0)
 
-GENES = ["CMY",\
-			"CTX-M",\
-			"GES",\
-			"IMP",\
-			"KPC",\
-			"NDM",\
-			"PER",\
-			"SHV",\
-			"TEM",\
-			"VEB",\
-			"VIM"]
+gene = snakemake.params.gene
+
+#GENES = ["CMY",\
+#			"CTX-M",\
+#			"GES",\
+#			"IMP",\
+#			"KPC",\
+#			"NDM",\
+#			"PER",\
+#			"SHV",\
+#			"TEM",\
+#			"VEB",\
+#			"VIM"]
 
 
-for gene in GENES:
-	with open("analysis/"+gene+"_accessions.txt", 'w') as f:
-		hits = metadata.loc[[(str(gene) in x) for x in list(metadata["Gene.hits"])]].index
-		for h in hits:
-			f.write("%s\n" % h)
+#for gene in GENES:
+with open(snakemake.output[0], 'w') as f:
+	hits = metadata.loc[[(str(gene) in x) for x in list(metadata["Gene.hits"])]].index
+	for h in hits:
+		f.write("%s\n" % h)
