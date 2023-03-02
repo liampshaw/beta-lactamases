456dfb713f579392698aa00915f5234734ac8637
Liam Shaw <liam.philip.shaw@gmail.com>
2023-03-02 11:54:36 +0000

    config with blast

--- scripts/pipeline/run_pipeline_rescomp.sh
+++ scripts/pipeline/run_pipeline_rescomp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #module load R/4.2.0-foss-2021b 
-echo "aat start of script"
+echo "at start of script, python is"
 which python
 
 
@@ -9,6 +9,7 @@ echo "Running on:"
 echo $MODULE_CPU_TYPE
 echo "at "`date`
 
+# Load different R module 
 if [ $MODULE_CPU_TYPE == "skylake" ]; then
         module load R/4.2.0-foss-2021b
 fi
@@ -27,35 +28,17 @@ conda activate betalactamases
 echo "after conda activate betalactamases"
 which python
 
-
-#module load R/4.2.0-foss-2021b 
-#export PYTHONPATH=/well/shaw/users/amu125/miniconda/$MODULE_CPU_TYPE/envs/betalactamases/bin/:${PYTHONPATH}
-#export PATH=/well/shaw/users/amu125/miniconda/$MODULE_CPU_TYPE/envs/betalactamases/bin/:${PATH}
-#which python
-#module load Anaconda3/2020.11
-#eval "$(conda shell.bash hook)"
-
 echo $PYTHONPATH
 echo $PATH
 which python
 
 # Crucial: add the correct python path
-export PATH=/well/shaw/users/amu125/miniconda/$MODULE_CPU_TYPE/envs/betalactamases/bin/:${PATH}
+# with blast location as well
+export PATH=/apps/well/ncbi-blast+/2.7.1/bin/:/well/shaw/users/amu125/miniconda/$MODULE_CPU_TYPE/envs/betalactamases/bin/:${PATH}
 which python
 # Crucial: add the location of the 'betalacatamases' site packages to the PYTHONPATH
 export PYTHONPATH=/well/shaw/users/amu125/miniconda/$MODULE_CPU_TYPE/envs/betalactamases/lib/python3.11/site-packages/:$PATH  
 
-# 
-#export PATH=/well/shaw/users/amu125/programs/ncbi-blast-2.13.0+/bin:$PATH
-#echo "after add blast to path"
-#which python
-
-# Julia path
-#export PATH=$HOME/bin:/usr/local/bin:/well/shaw/users/amu125/programs/julia-1.7.2/bin:/well/shaw/users/amu125/programs/pangraph/bin:$PATH
-#export JULIA_DEPOT_PATH="/well/shaw/users/amu125/programs/julia-1.7.2/depot:$JULIA_DEPOT_PATH"
-
-#echo "after julia paths"
-#which python
 
 #snakemake -R --until download_CARD_DB --cores 1 --rerun-incomplete
 #snakemake -R --until download_CARD_DB --cores 1 --rerun-incomplete
