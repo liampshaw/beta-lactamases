9236141388a328c4066e717d0491d7cdbaa14967
Liam Shaw <liam.philip.shaw@gmail.com>
2023-03-14 14:40:24 +0000

    minor: comment out python checks

--- scripts/pipeline/run_pipeline_rescomp.sh
+++ scripts/pipeline/run_pipeline_rescomp.sh
@@ -2,7 +2,7 @@
 
 #module load R/4.2.0-foss-2021b 
 echo "at start of script, python is"
-which python
+#which python
 
 
 echo "Running on:"
@@ -22,20 +22,21 @@ module load Anaconda3/2020.11
 eval "$(conda shell.bash hook)"
 conda deactivate
 echo "after conda deactivate"
-which python
+#which python
 
 conda activate betalactamases
 echo "after conda activate betalactamases"
-which python
+#which python
 
-echo $PYTHONPATH
-echo $PATH
-which python
+#echo $PYTHONPATH
+#echo $PATH
+#which python
 
 # Crucial: add the correct python path
 # with blast location as well
 export PATH=/apps/well/ncbi-blast+/2.7.1/bin/:/well/shaw/users/amu125/miniconda/$MODULE_CPU_TYPE/envs/betalactamases/bin/:${PATH}
-which python
+#which python
+
 # Crucial: add the location of the 'betalacatamases' site packages to the PYTHONPATH
 export PYTHONPATH=/well/shaw/users/amu125/miniconda/$MODULE_CPU_TYPE/envs/betalactamases/lib/python3.11/site-packages/:$PATH  
 
