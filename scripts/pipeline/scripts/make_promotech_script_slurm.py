434b82f255b53bc4af025b02c0f1b49746380dcf
Liam Shaw <liam.philip.shaw@gmail.com>
2023-03-20 12:45:29 +0000

    feat: promotech script generator for slurm

---
+++ scripts/pipeline/scripts/make_promotech_script_slurm.py
@@ -0,0 +1,26 @@
+import argparse
+
+
+def get_options():
+    parser = argparse.ArgumentParser(description='Creates slurm cluster jobs for running PromoTech on multifasta file.')
+    parser.add_argument('--fasta_list', help='List of fasta files', type=str)
+    return parser.parse_args()
+
+def main():
+	args = get_options()
+	print(args)
+	print("#!/bin/bash")
+	print("module load Anaconda3/2020.11")
+	print('eval "$(conda shell.bash hook)"')
+	print('conda activate promotech_env')
+	print('outdir=tmp-$SLURM_ARRAY_TASK_ID')
+	print('fasta=$(sed "${SLURM_ARRAY_TASK_ID}q;d" '+args.fasta_list)
+	print('echo $name')
+	print('time python promotech.py -pg -m RF-HOT -f $fasta -o $outdir')
+	print('time python promotech.py -g -t 0.6 -i $outdir -o $outdir')
+	print('rm $outdir/*data')
+	print('mv $outdir/genome_predictions.csv $outdir/"$accession"-promoter_predictions.tsv')
+	
+
+if __name__=="__main__":
+    main()
