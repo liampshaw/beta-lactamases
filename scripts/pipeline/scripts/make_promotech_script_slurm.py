5584da5c4098f4a40564467841deb46abc407517
Liam Shaw <liam.philip.shaw@gmail.com>
2023-03-20 12:49:09 +0000

    dirs

--- scripts/pipeline/scripts/make_promotech_script_slurm.py
+++ scripts/pipeline/scripts/make_promotech_script_slurm.py
@@ -13,13 +13,15 @@ def main():
 	print("module load Anaconda3/2020.11")
 	print('eval "$(conda shell.bash hook)"')
 	print('conda activate promotech_env')
+	print('overall_outdir=promoter_predictions')
 	print('outdir=tmp-$SLURM_ARRAY_TASK_ID')
 	print('fasta=$(sed "${SLURM_ARRAY_TASK_ID}q;d" '+args.fasta_list)
 	print('echo $name')
 	print('time python /well/shaw/users/amu125/programs/PromoTech/promotech.py -pg -m RF-HOT -f $fasta -o $outdir')
 	print('time python /well/shaw/users/amu125/programs/PromoTech/promotech.py -g -t 0.6 -i $outdir -o $outdir')
 	print('rm $outdir/*data')
-	print('mv $outdir/genome_predictions.csv $outdir/"$accession"-promoter_predictions.tsv')
+	print('mv $outdir/genome_predictions.csv $overall_outdir/"$accession"-promoter_predictions.tsv')
+	print('rm -r $outdir/*')
 	
 
 if __name__=="__main__":
